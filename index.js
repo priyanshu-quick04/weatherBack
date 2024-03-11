7b8039c47fb91cc35c1f9d7fa1f8548b68f9ed28
PRIYANSHU GUNWANT <122587763+priyanshu-quick04@users.noreply.github.com>
2024-03-12 01:21:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,16 +32,9 @@ app.use(cors(
   {
     origin:["https://weather-frontend-liart.vercel.app"],
     methods:["POST","GET"],
-    credentials: true
+    credentials: false
 ));
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 
 
 app.get("/", (req, res) => {
