620a32c7769f3277224a9f466652ef2625c19522
PRIYANSHU GUNWANT <122587763+priyanshu-quick04@users.noreply.github.com>
2024-03-12 18:28:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,15 +28,12 @@ const User = new mongoose.model("User", userSchema);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
-const corsOptions ={
-   origin:'*', 
-   credentials:true,      
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions))
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://weather-frontend-liart.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
